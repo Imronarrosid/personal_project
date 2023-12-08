2a2f80e79877c96bf877818a28d769a455e396dc
Imron Arrosid <imronbaru007@gmail.com>
2023-12-08 12:15:07 +0700

    change snackbar

--- lib/presentation/shared_components/custom_snackbar.dart
+++ lib/presentation/shared_components/custom_snackbar.dart
@@ -1,41 +1,30 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:personal_project/constant/color.dart';
 import 'package:personal_project/constant/dimens.dart';
 import 'package:personal_project/presentation/l10n/stings.g.dart';
 
 void showUploadingSnackBar(BuildContext context) {
-  final snackBar = SnackBar(
-    content: Text(
-      LocaleKeys.message_uploading.tr(),
-      textAlign: TextAlign.center,
-    ),
-    backgroundColor: COLOR_black_ff121212.withOpacity(0.4),
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-        left: Dimens.DIMENS_80,
-        right: Dimens.DIMENS_80,
-        bottom: Dimens.DIMENS_70),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  Fluttertoast.showToast(
+      msg: LocaleKeys.message_uploading.tr(),
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: COLOR_black_ff121212,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
 
 void showUploadedSnackBar(BuildContext context) {
-  final snackBar = SnackBar(
-    content: Text(
-      LocaleKeys.message_uploaded.tr(),
-      textAlign: TextAlign.center,
-    ),
-    backgroundColor: COLOR_black_ff121212.withOpacity(0.4),
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-        left: Dimens.DIMENS_80,
-        right: Dimens.DIMENS_80,
-        bottom: Dimens.DIMENS_70),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  Fluttertoast.showToast(
+      msg: LocaleKeys.message_uploaded.tr(),
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: COLOR_black_ff121212,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
 
 void showLoginSuccessSnackBar(BuildContext context) {
@@ -56,55 +45,34 @@ void showLoginSuccessSnackBar(BuildContext context) {
 }
 
 void showLoginErrorSnackBar(BuildContext context) {
-  final snackBar = SnackBar(
-    content: const Text(
-      'Login error',
-      textAlign: TextAlign.center,
-    ),
-    backgroundColor: COLOR_black_ff121212.withOpacity(0.4),
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-        left: Dimens.DIMENS_80,
-        right: Dimens.DIMENS_80,
-        bottom: Dimens.DIMENS_70),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  Fluttertoast.showToast(
+      msg: 'Login error',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: COLOR_black_ff121212,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
 
 void showNoInternetSnackBar(BuildContext context) {
-  final snackBar = SnackBar(
-    content: const Text(
-      'No internet',
-      textAlign: TextAlign.center,
-    ),
-    backgroundColor: COLOR_black_ff121212.withOpacity(0.4),
-    behavior: SnackBarBehavior.floating,
-    elevation: 0,
-    duration: const Duration(milliseconds: 600),
-    margin: EdgeInsets.only(
-        left: Dimens.DIMENS_80,
-        right: Dimens.DIMENS_80,
-        bottom: MediaQuery.of(context).size.height * 0.8),
-    shape: const StadiumBorder(),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  Fluttertoast.showToast(
+      msg: 'No internet',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: COLOR_black_ff121212,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
 
 void showLoginFailedSnackBar(BuildContext context) {
-  final snackBar = SnackBar(
-    content: const Text(
-      'Login gagal',
-      textAlign: TextAlign.center,
-    ),
-    backgroundColor: COLOR_black_ff121212.withOpacity(0.4),
-    behavior: SnackBarBehavior.floating,
-    elevation: 0,
-    margin: EdgeInsets.only(
-        left: Dimens.DIMENS_80,
-        right: Dimens.DIMENS_80,
-        bottom: Dimens.DIMENS_70),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  Fluttertoast.showToast(
+      msg: 'Login gagal',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: COLOR_black_ff121212,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
