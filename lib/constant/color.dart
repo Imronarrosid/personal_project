3b95e373d82968f6f9f0384eb6ac6f61bdba5790
Imron Arrosid <imronbaru007@gmail.com>
2023-12-11 19:30:49 +0700

    add new color

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -3,7 +3,24 @@
 import 'package:flutter/material.dart';
 
 Color COLOR_black_ff121212 = const Color(0xFF121212);
+int _COLOR_black_ff121212 = 0xFF121212;
 Color COLOR_black = Colors.black;
 Color COLOR_white_fff5f5f5 = const Color(0xFFf5f5f5);
 Color COLOR_red = Colors.red;
 Color COLOR_grey = const Color(0xFFdddddd);
+
+MaterialColor PRIMARY = MaterialColor(
+  _COLOR_black_ff121212,
+  <int, Color>{
+    50: COLOR_black_ff121212.withOpacity(0.05),
+    100: COLOR_black_ff121212.withOpacity(0.1),
+    200: COLOR_black_ff121212.withOpacity(0.2),
+    300: COLOR_black_ff121212.withOpacity(0.3),
+    400: COLOR_black_ff121212.withOpacity(0.4),
+    500: COLOR_black_ff121212.withOpacity(0.5),
+    600: COLOR_black_ff121212.withOpacity(0.6),
+    700: COLOR_black_ff121212.withOpacity(0.7),
+    800: COLOR_black_ff121212.withOpacity(0.8),
+    900: COLOR_black_ff121212.withOpacity(0.9),
+  },
+);
