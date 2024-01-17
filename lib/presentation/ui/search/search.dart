8c6217ccebffbd47dbba4c4ab300ec46ae425799
Imron Arrosid <imronbaru007@gmail.com>
2024-01-17 13:29:54 +0700

    fixs focus behaviour

--- lib/presentation/ui/search/search.dart
+++ lib/presentation/ui/search/search.dart
@@ -23,6 +23,7 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _textEditingController = TextEditingController();
   Timer? _debounce;
+  FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -34,83 +35,92 @@ class _SearchPageState extends State<SearchPage> {
         child: Builder(builder: (context) {
           return GestureDetector(
             onTap: () => FocusScope.of(context).unfocus(),
-            child: Scaffold(
-              appBar: AppBar(
-                  elevation: 0,
-                  toolbarHeight: 80,
-                  title: Container(
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.tertiary,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: TextField(
-                      controller: _textEditingController,
-                      onChanged: (query) {
-                        final searchBloc = BlocProvider.of<SearchBloc>(context);
-                        if (_debounce?.isActive ?? false) _debounce?.cancel();
-                        _debounce =
-                            Timer(const Duration(milliseconds: 500), () {
-                          // do something with query
-                          searchBloc.add(SearchEvent(query));
-                        });
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: LocaleKeys.label_search.tr(),
-                        contentPadding: const EdgeInsets.all(5),
-                        suffixIcon: GestureDetector(
-                            onTap: () {
-                              _textEditingController.clear();
-                            },
-                            child: Icon(MdiIcons.close)),
-                        suffixIconColor: COLOR_grey,
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(10)),
+            child: FocusScope(
+              node: FocusScopeNode(),
+              child: Scaffold(
+                appBar: AppBar(
+                    elevation: 0,
+                    toolbarHeight: 80,
+                    title: Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.tertiary,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: TextField(
+                        focusNode: _focusNode,
+                        controller: _textEditingController,
+                        onChanged: (query) {
+                          final searchBloc =
+                              BlocProvider.of<SearchBloc>(context);
+                          if (_debounce?.isActive ?? false) _debounce?.cancel();
+                          _debounce =
+                              Timer(const Duration(milliseconds: 500), () {
+                            // do something with query
+                            searchBloc.add(SearchEvent(query));
+                          });
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.search),
+                          hintText: LocaleKeys.label_search.tr(),
+                          contentPadding: const EdgeInsets.all(5),
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                _textEditingController.clear();
+                              },
+                              child: Icon(MdiIcons.close)),
+                          suffixIconColor: COLOR_grey,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
                       ),
-                    ),
-                  )),
-              body: BlocBuilder<SearchBloc, SearchState>(
-                builder: (context, state) {
-                  if (state.status == SearchStatus.loading) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (state.status == SearchStatus.noItemFound) {
-                    return Center(
-                        child: Text(LocaleKeys.message_not_found
-                            .tr(args: [_textEditingController.text])));
-                  }
-                  if (state.status == SearchStatus.success) {
-                    return ListView.builder(
-                        itemCount: state.results!.length,
-                        itemBuilder: (ctx, index) {
-                          final result = state.results![index];
-                          return ListTile(
-                            tileColor: Colors.transparent,
-                            onTap: () {
-                              debugPrint('photo${result.photo}');
-                              context.push(APP_PAGE.profile.toPath,
-                                  extra: ProfilePayload(
-                                    uid: result.id,
-                                    name: result.name!,
-                                    userName: result.userName!,
-                                    photoURL: result.photo!,
-                                  ));
-                              debugPrint('profile');
-                            },
-                            leading: CircleAvatar(
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.tertiary,
-                              backgroundImage: CachedNetworkImageProvider(
-                                result.photo!,
+                    )),
+                body: BlocBuilder<SearchBloc, SearchState>(
+                  builder: (context, state) {
+                    if (state.status == SearchStatus.loading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (state.status == SearchStatus.noItemFound) {
+                      return Center(
+                          child: Text(LocaleKeys.message_not_found
+                              .tr(args: [_textEditingController.text])));
+                    }
+                    if (state.status == SearchStatus.success) {
+                      return ListView.builder(
+                          itemCount: state.results!.length,
+                          itemBuilder: (ctx, index) {
+                            final result = state.results![index];
+                            return ListTile(
+                              tileColor: Colors.transparent,
+                              onTap: () {
+                                debugPrint('photo${result.photo}');
+                                context.push(APP_PAGE.profile.toPath,
+                                    extra: ProfilePayload(
+                                      uid: result.id,
+                                      name: result.name!,
+                                      userName: result.userName!,
+                                      photoURL: result.photo!,
+                                    ));
+                                if (FocusManager.instance.primaryFocus !=
+                                    null) {
+                                  FocusManager.instance.primaryFocus!.unfocus();
+                                }
+                                debugPrint('profile');
+                              },
+                              leading: CircleAvatar(
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.tertiary,
+                                backgroundImage: CachedNetworkImageProvider(
+                                  result.photo!,
+                                ),
                               ),
-                            ),
-                            title: Text(result.name!),
-                            subtitle: Text(result.userName!),
-                          );
-                        });
-                  }
-                  return Container();
-                },
+                              title: Text(result.name!),
+                              subtitle: Text(result.userName!),
+                            );
+                          });
+                    }
+                    return Container();
+                  },
+                ),
               ),
             ),
           );
