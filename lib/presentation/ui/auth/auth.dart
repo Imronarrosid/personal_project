1325409977009be2838cf34bfe88c9ce06f1cfd4
Imron Arrosid <imronbaru007@gmail.com>
2024-10-08 14:59:46 +0700

    add popup dialog for desktop view

--- lib/presentation/ui/auth/auth.dart
+++ lib/presentation/ui/auth/auth.dart
@@ -8,117 +8,232 @@ import 'package:personal_project/constant/dimens.dart';
 import 'package:personal_project/constant/font_size.dart';
 import 'package:personal_project/presentation/assets/images.dart';
 import 'package:personal_project/presentation/l10n/stings.g.dart';
+import 'package:personal_project/presentation/responsive/dimension.dart';
 import 'package:personal_project/presentation/shared_components/custom_snackbar.dart';
 import 'package:personal_project/presentation/ui/auth/bloc/auth_bloc.dart';
 import 'package:personal_project/utils/check_network.dart';
 
 void showAuthBottomSheetFunc(BuildContext context) {
-  showModalBottomSheet(
-      context: context,
-      enableDrag: true,
-      elevation: 4,
-      isScrollControlled: true,
-      builder: (context) {
-        return Container(
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
-              ),
-              color: Theme.of(context).colorScheme.background),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.7,
-          child: SingleChildScrollView(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.all(Dimens.DIMENS_10),
-                    child: IconButton(
-                      padding: const EdgeInsets.all(0),
-                      icon: const Icon(Icons.close_rounded),
-                      onPressed: () {
-                        context.pop();
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: Dimens.DIMENS_45,
-                  ),
-                  Text(
-                    LocaleKeys.label_signin.tr(),
-                    style: TextStyle(
-                        fontSize: FontSize.FONT_SIZE_24,
-                        fontWeight: FontWeight.w900),
-                  ),
-                  SizedBox(
-                    height: Dimens.DIMENS_10,
-                  ),
-                  Text(
-                    LocaleKeys.message_login_for_more_experience.tr(),
-                    style: Theme.of(context).textTheme.bodyMedium!.apply(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .onSurface
-                              .withOpacity(0.6),
+  if (MediaQuery.of(context).size.width > mobileWidth) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: SizedBox(
+              width: 400,
+              height: 500,
+              child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.all(Dimens.DIMENS_10),
+                        child: IconButton(
+                          padding: const EdgeInsets.all(0),
+                          icon: const Icon(Icons.close_rounded),
+                          onPressed: () {
+                            context.pop();
+                          },
                         ),
-                  ),
-                  SizedBox(
-                    height: Dimens.DIMENS_70,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: Dimens.DIMENS_20),
-                    child: Material(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      child: InkWell(
-                        splashColor: COLOR_grey,
-                        borderRadius: BorderRadius.circular(50),
-                        onTap: () {
-                          BlocProvider.of<AuthBloc>(context)
-                              .add(LogInWithGoogle());
-                        },
-                        child: Container(
-                          height: Dimens.DIMENS_45,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: Dimens.DIMENS_8,
-                              vertical: Dimens.DIMENS_8),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onSurface
-                                    .withOpacity(0.4),
-                              ),
+                      ),
+                      SizedBox(
+                        height: Dimens.DIMENS_45,
+                      ),
+                      Text(
+                        LocaleKeys.label_signin.tr(),
+                        style: TextStyle(
+                            fontSize: FontSize.FONT_SIZE_24,
+                            fontWeight: FontWeight.w900),
+                      ),
+                      SizedBox(
+                        height: Dimens.DIMENS_10,
+                      ),
+                      Text(
+                        LocaleKeys.message_login_for_more_experience.tr(),
+                        style: Theme.of(context).textTheme.bodyMedium!.apply(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurface
+                                  .withOpacity(0.6),
+                            ),
+                      ),
+                      SizedBox(
+                        height: Dimens.DIMENS_70,
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: Dimens.DIMENS_20),
+                        child: Material(
+                          shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(50)),
-                          child: Stack(
-                            children: [
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: SvgPicture.asset(
-                                  Images.IC_GOOLE,
-                                  width: Dimens.DIMENS_34,
-                                ),
+                          child: InkWell(
+                            splashColor: COLOR_grey,
+                            borderRadius: BorderRadius.circular(50),
+                            onTap: () {
+                              BlocProvider.of<AuthBloc>(context)
+                                  .add(LogInWithGoogle());
+                            },
+                            child: Container(
+                              height: Dimens.DIMENS_45,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: Dimens.DIMENS_8,
+                                  vertical: Dimens.DIMENS_8),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSurface
+                                        .withOpacity(0.4),
+                                  ),
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: Stack(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: SvgPicture.asset(
+                                      Images.IC_GOOLE,
+                                      width: Dimens.DIMENS_34,
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      LocaleKeys.label_signin_with_google.tr(),
+                                      style: TextStyle(
+                                          fontSize: FontSize.FONT_SIZE_16,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  )
+                                ],
                               ),
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  LocaleKeys.label_signin_with_google.tr(),
-                                  style: TextStyle(
-                                      fontSize: FontSize.FONT_SIZE_16,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              )
-                            ],
+                            ),
                           ),
                         ),
                       ),
+                    ]),
+              ),
+            ),
+          );
+        });
+  } else {
+    showModalBottomSheet(
+        context: context,
+        enableDrag: true,
+        elevation: 4,
+        isScrollControlled: true,
+        builder: (context) {
+          return const GoogleSignIn();
+        });
+  }
+}
+
+class GoogleSignIn extends StatelessWidget {
+  const GoogleSignIn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+          ),
+          color: Theme.of(context).colorScheme.background),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(Dimens.DIMENS_10),
+                child: IconButton(
+                  padding: const EdgeInsets.all(0),
+                  icon: const Icon(Icons.close_rounded),
+                  onPressed: () {
+                    context.pop();
+                  },
+                ),
+              ),
+              SizedBox(
+                height: Dimens.DIMENS_45,
+              ),
+              Text(
+                LocaleKeys.label_signin.tr(),
+                style: TextStyle(
+                    fontSize: FontSize.FONT_SIZE_24,
+                    fontWeight: FontWeight.w900),
+              ),
+              SizedBox(
+                height: Dimens.DIMENS_10,
+              ),
+              Text(
+                LocaleKeys.message_login_for_more_experience.tr(),
+                style: Theme.of(context).textTheme.bodyMedium!.apply(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .onSurface
+                          .withOpacity(0.6),
+                    ),
+              ),
+              SizedBox(
+                height: Dimens.DIMENS_70,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Dimens.DIMENS_20),
+                child: Material(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  child: InkWell(
+                    splashColor: COLOR_grey,
+                    borderRadius: BorderRadius.circular(50),
+                    onTap: () {
+                      BlocProvider.of<AuthBloc>(context).add(LogInWithGoogle());
+                    },
+                    child: Container(
+                      height: Dimens.DIMENS_45,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: Dimens.DIMENS_8,
+                          vertical: Dimens.DIMENS_8),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSurface
+                                .withOpacity(0.4),
+                          ),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: SvgPicture.asset(
+                              Images.IC_GOOLE,
+                              width: Dimens.DIMENS_34,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              LocaleKeys.label_signin_with_google.tr(),
+                              style: TextStyle(
+                                  fontSize: FontSize.FONT_SIZE_16,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ]),
-          ),
-        );
-      });
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
 }
