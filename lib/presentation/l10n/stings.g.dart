275be524d1203ee7f509c8058e38c46cb4e50251
Imron Arrosid <imronbaru007@gmail.com>
2024-10-22 15:10:25 +0700

    new keys

--- lib/presentation/l10n/stings.g.dart
+++ lib/presentation/l10n/stings.g.dart
@@ -13,6 +13,7 @@ abstract class  LocaleKeys {
   static const label_home = 'label_home';
   static const label_search = 'label_search';
   static const label_message = 'label_message';
+  static const label_messages = 'label_messages';
   static const label_profile = 'label_profile';
   static const label_60_second = 'label_60_second';
   static const label_flash = 'label_flash';
