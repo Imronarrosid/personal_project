56e253fb59de39203f8932c6681a1d18bc1b3e49
Imron Arrosid <imronbaru007@gmail.com>
2024-11-14 21:39:56 +0700

    update get DateTime from firestore

--- lib/utils/get_ntp_time.dart
+++ lib/utils/get_ntp_time.dart
@@ -1,16 +1,22 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:personal_project/domain/services/firebase/firebase_service.dart';
+import 'package:personal_project/utils/debug_mode_print.dart';
 
 Future<DateTime> fetchTime() async {
-  final response = await http
-      .get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Jakarta'));
-
-  if (response.statusCode == 200) {
-    var data = jsonDecode(response.body);
-    String datetime = data['datetime']; // Fetch the Jakarta time
-    return DateTime.parse(datetime);
-  } else {
-    throw Exception('Failed to load time');
-  }
+  DocumentReference ref = firebaseFirestore
+      .collection('users')
+      .doc(firebaseAuth.currentUser!.uid)
+      .collection('dateTime')
+      .doc('dateTime');
+  await ref.set({'dateTime': FieldValue.serverTimestamp()});
+  DateTime dateTime = await ref.get().then(
+    (value) {
+      debugModePrint((value.data() as Map<String, dynamic>)['dateTime']);
+      Timestamp timeStamp =
+          ((value.data() as Map<String, dynamic>)['dateTime']);
+      debugModePrint(timeStamp.toDate());
+      return timeStamp.toDate();
+    },
+  );
+  return dateTime;
 }
