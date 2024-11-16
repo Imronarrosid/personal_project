6955c40c197396d30ce73b8c9a0f02ec727771be
Imron Arrosid <imronbaru007@gmail.com>
2024-11-17 06:34:56 +0700

    update ditname modal

--- lib/presentation/ui/edit_profile/edit_modal/edit_name_modal.dart
+++ lib/presentation/ui/edit_profile/edit_modal/edit_name_modal.dart
@@ -10,6 +10,9 @@ import 'package:personal_project/presentation/responsive/dimension.dart';
 import 'package:personal_project/presentation/ui/edit_profile/cubit/edit_name_cubit.dart';
 import 'package:personal_project/utils/edit_name_check.dart';
 import 'package:personal_project/utils/is_same_day.dart';
+import 'package:solar_icons/solar_icons.dart';
+
+import '../../../../utils/debug_mode_print.dart';
 
 void showEditNameModal(BuildContext context, String name, Timestamp timestamp,
     Timestamp userCreatedAt) async {
@@ -31,69 +34,201 @@ void showEditNameModal(BuildContext context, String name, Timestamp timestamp,
   int daysCount = await daysUntilOneWeeks(timestamp);
   if (context.mounted && MediaQuery.of(context).size.width > mobileWidth) {
     showDialog(
-        context: context,
-        builder: (context) => Dialog(
-              surfaceTintColor: Colors.transparent,
-              backgroundColor: Theme.of(context).colorScheme.background,
-              child: Container(
+      context: context,
+      builder: (context) => Dialog(
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: SizedBox(
+          width: 400,
+          height: 300,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: EditNameView(
+                  formKey: formKey,
+                  validator: validator,
+                  controller: controller,
+                  isCanEdit: isCanEdit,
+                  daysCount: daysCount,
+                  name: name,
+                  isShowDragHandle: false,
                   width: 400,
                   height: 300,
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: Dimens.DIMENS_6,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () => context.pop(),
+                      icon: const Icon(
+                        Icons.close_rounded,
+                      )),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  } else {
+    if (!context.mounted) return;
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        isScrollControlled: true,
+        elevation: 0,
+        context: context,
+        builder: (context) {
+          return EditNameView(
+            formKey: formKey,
+            validator: validator,
+            controller: controller,
+            isCanEdit: isCanEdit,
+            daysCount: daysCount,
+            name: name,
+            height: 250,
+          );
+        });
+  }
+}
+
+class EditNameView extends StatefulWidget {
+  final GlobalKey<FormState> formKey;
+  final String? Function(String? value) validator;
+  final TextEditingController controller;
+  final bool isCanEdit;
+  final int daysCount;
+  final String name;
+  final bool isShowDragHandle;
+  final double? height;
+  final double? width;
+  const EditNameView({
+    super.key,
+    required this.formKey,
+    required this.validator,
+    required this.controller,
+    required this.isCanEdit,
+    required this.daysCount,
+    required this.name,
+    this.isShowDragHandle = true,
+    this.height,
+    this.width,
+  });
+
+  @override
+  State<EditNameView> createState() => _EditNameViewState();
+}
+
+class _EditNameViewState extends State<EditNameView> {
+  late final ValueNotifier<String> valueListenableBuilder;
+
+  @override
+  void initState() {
+    valueListenableBuilder = ValueNotifier<String>(widget.controller.text);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<EditNameCubit, EditNameState>(
+      listener: (context, state) {
+        if (state.status == EditNameStatus.nameEditSuccess) {
+          context.pop();
+        }
+      },
+      child: Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: EdgeInsets.all(Dimens.DIMENS_12),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            height: widget.height,
+            width: widget.width,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              widget.isShowDragHandle
+                  ? Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: Dimens.DIMENS_50,
+                        height: Dimens.DIMENS_5,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.tertiary,
+                            borderRadius: BorderRadius.circular(50)),
                       ),
-                      Text(
-                        LocaleKeys.label_name.tr(),
-                        style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: Dimens.DIMENS_8,
-                      ),
-                      Form(
-                        key: formKey,
-                        child: TextFormField(
-                          validator: validator,
-                          controller: controller,
-                          maxLength: 24,
-                          buildCounter: (context,
-                              {required currentLength,
-                              required isFocused,
-                              required maxLength}) {
-                            return const Text('');
-                          },
-                          onChanged: (value) {
-                            formKey.currentState!.validate();
-                          },
-                          enabled: isCanEdit,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                        ),
-                      ),
-                      SizedBox(
-                        height: Dimens.DIMENS_8,
-                      ),
-                      isCanEdit
-                          ? Text(
-                              LocaleKeys.message_edit_can_name_every_7_day.tr())
-                          : Text(LocaleKeys.message_edit_name_day_later
-                              .tr(args: [daysCount.toString()])),
-                      SizedBox(
-                        height: Dimens.DIMENS_18,
-                      ),
-                      InkWell(
-                        onTap: isCanEdit
+                    )
+                  : const SizedBox.shrink(),
+              SizedBox(
+                height: Dimens.DIMENS_6,
+              ),
+              Text(
+                LocaleKeys.label_name.tr(),
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: Dimens.DIMENS_8,
+              ),
+              Form(
+                key: widget.formKey,
+                child: TextFormField(
+                  validator: widget.validator,
+                  controller: widget.controller,
+                  maxLength: 24,
+                  buildCounter: (context,
+                      {required currentLength,
+                      required isFocused,
+                      required maxLength}) {
+                    return const SizedBox.shrink();
+                  },
+                  onChanged: (value) {
+                    widget.formKey.currentState!.validate();
+                    valueListenableBuilder.value = value.trim();
+                  },
+                  enabled: widget.isCanEdit,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              SizedBox(
+                height: Dimens.DIMENS_8,
+              ),
+              widget.isCanEdit
+                  ? Text(LocaleKeys.message_edit_can_name_every_7_day.tr())
+                  : Text(
+                      LocaleKeys.message_edit_name_day_later
+                          .tr(args: [widget.daysCount.toString()]),
+                    ),
+              SizedBox(
+                height: Dimens.DIMENS_18,
+              ),
+              const Spacer(),
+              ValueListenableBuilder(
+                  valueListenable: valueListenableBuilder,
+                  builder: (context, value, child) {
+                    return Material(
+                      color: widget.isCanEdit && widget.name != value
+                          ? Theme.of(context).colorScheme.primary
+                          : Theme.of(context).colorScheme.tertiary,
+                      borderRadius: BorderRadius.circular(50),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(50),
+                        onTap: widget.isCanEdit && widget.name != value
                             ? () {
-                                if (formKey.currentState!.validate() &&
-                                    name != controller.text) {
+                                if (widget.formKey.currentState!.validate() &&
+                                    widget.name != widget.controller.text) {
                                   BlocProvider.of<EditNameCubit>(context)
-                                      .editName(controller.text);
-                                  debugPrint('editname');
+                                      .editName(widget.controller.text);
+                                  debugModePrint('editname');
                                 }
                               }
                             : null,
@@ -102,13 +237,18 @@ void showEditNameModal(BuildContext context, String name, Timestamp timestamp,
                           height: Dimens.DIMENS_38,
                           alignment: Alignment.center,
                           decoration: BoxDecoration(
-                              color: isCanEdit
+                            border: Border.all(
+                              color: widget.isCanEdit &&
+                                      widget.name !=
+                                          widget.controller.text.trim()
                                   ? Theme.of(context).colorScheme.primary
                                   : Theme.of(context).colorScheme.tertiary,
-                              borderRadius: BorderRadius.circular(50)),
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
                           child: BlocBuilder<EditNameCubit, EditNameState>(
                             builder: (context, state) {
-                              debugPrint('state ${state.status}');
+                              debugModePrint('state ${state.status}');
                               if (state.status == EditNameStatus.editProccess) {
                                 return SizedBox(
                                   width: Dimens.DIMENS_18,
@@ -122,158 +262,23 @@ void showEditNameModal(BuildContext context, String name, Timestamp timestamp,
                               return Text(
                                 LocaleKeys.label_save.tr(),
                                 style: TextStyle(
-                                    color: isCanEdit &&
-                                            name != controller.text.trim()
-                                        ? Theme.of(context)
-                                            .colorScheme
-                                            .secondary
-                                        : COLOR_black_ff121212),
+                                  color: widget.isCanEdit &&
+                                          widget.name !=
+                                              widget.controller.text.trim()
+                                      ? Theme.of(context).colorScheme.secondary
+                                      : COLOR_black_ff121212,
+                                ),
                               );
                             },
                           ),
                         ),
                       ),
-                    ],
-                  )),
-            ));
-  } else {
-    showModalBottomSheet(
-        backgroundColor: Colors.transparent,
-        isScrollControlled: true,
-        elevation: 0,
-        context: context,
-        builder: (context) {
-          return editNameView(context, formKey, validator, controller,
-              isCanEdit, daysCount, name);
-        });
-  }
-}
-
-BlocListener<EditNameCubit, EditNameState> editNameView(
-    BuildContext context,
-    GlobalKey<FormState> formKey,
-    String? validator(String? value),
-    TextEditingController controller,
-    bool isCanEdit,
-    int daysCount,
-    String name) {
-  return BlocListener<EditNameCubit, EditNameState>(
-    listener: (context, state) {
-      if (state.status == EditNameStatus.nameEditSuccess) {
-        context.pop();
-      }
-    },
-    child: Padding(
-      padding:
-          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          padding: EdgeInsets.all(Dimens.DIMENS_12),
-          decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.background,
-              borderRadius: BorderRadius.circular(10)),
-          height: 250,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: Dimens.DIMENS_50,
-                height: Dimens.DIMENS_5,
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.tertiary,
-                    borderRadius: BorderRadius.circular(50)),
-              ),
-            ),
-            SizedBox(
-              height: Dimens.DIMENS_6,
-            ),
-            Text(
-              LocaleKeys.label_name.tr(),
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: Dimens.DIMENS_8,
-            ),
-            Form(
-              key: formKey,
-              child: TextFormField(
-                validator: validator,
-                controller: controller,
-                maxLength: 24,
-                buildCounter: (context,
-                    {required currentLength,
-                    required isFocused,
-                    required maxLength}) {
-                  return const Text('');
-                },
-                onChanged: (value) {
-                  formKey.currentState!.validate();
-                },
-                enabled: isCanEdit,
-                autofocus: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-              ),
-            ),
-            SizedBox(
-              height: Dimens.DIMENS_8,
-            ),
-            isCanEdit
-                ? Text(LocaleKeys.message_edit_can_name_every_7_day.tr())
-                : Text(LocaleKeys.message_edit_name_day_later
-                    .tr(args: [daysCount.toString()])),
-            SizedBox(
-              height: Dimens.DIMENS_18,
-            ),
-            InkWell(
-              onTap: isCanEdit
-                  ? () {
-                      if (formKey.currentState!.validate() &&
-                          name != controller.text) {
-                        BlocProvider.of<EditNameCubit>(context)
-                            .editName(controller.text);
-                        debugPrint('editname');
-                      }
-                    }
-                  : null,
-              child: Container(
-                width: double.infinity,
-                height: Dimens.DIMENS_38,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: isCanEdit
-                        ? Theme.of(context).colorScheme.primary
-                        : Theme.of(context).colorScheme.tertiary,
-                    borderRadius: BorderRadius.circular(50)),
-                child: BlocBuilder<EditNameCubit, EditNameState>(
-                  builder: (context, state) {
-                    debugPrint('state ${state.status}');
-                    if (state.status == EditNameStatus.editProccess) {
-                      return SizedBox(
-                        width: Dimens.DIMENS_18,
-                        height: Dimens.DIMENS_18,
-                        child: CircularProgressIndicator(
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      );
-                    }
-                    return Text(
-                      LocaleKeys.label_save.tr(),
-                      style: TextStyle(
-                          color: isCanEdit && name != controller.text.trim()
-                              ? Theme.of(context).colorScheme.secondary
-                              : COLOR_black_ff121212),
                     );
-                  },
-                ),
-              ),
-            ),
-          ]),
+                  }),
+            ]),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
